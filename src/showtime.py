3c718469e0ebc6195bace87096cf0b31c5fce711
ishanisen <ishanisen0610@gmail.com>
2025-11-14 13:49:55 -0400

    showtime class created

--- src/showtime.py
+++ src/showtime.py
@@ -1,1 +1,15 @@
+from datetime import time
+class showtime:
+    def __init__(self, showtimeID: str, startAt: time):
+        self.showtimeID = showtime
+        self.startAt = startAt
+    
+    def __str__(self):
+        return f"{self.showtimeID} at {self.startAt}"
 
+    def read(cls):
+        showtimeID = input("Eneter Show Time ID: ")
+        startAt = input ("Enter the Start Time of the Movie:")
+        return cls(showtimeId = showtimeID, startAt=startAt)
+    
+    
